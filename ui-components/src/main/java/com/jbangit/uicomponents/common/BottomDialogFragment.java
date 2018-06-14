78ae9d5f362ea07ae660352bda006bfcc3c25a27
yuanjunyao <yuanjunyao@jbangit.com>
2018-06-14 16:45:52 +0800

    feat(common): 新增底部弹出对话框

---
+++ ui-components/src/main/java/com/jbangit/uicomponents/common/BottomDialogFragment.java
@@ -0,0 +1,53 @@
+package com.jbangit.uicomponents.common;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.jbangit.uicomponents.R;
+
+import java.util.Objects;
+
+public abstract class BottomDialogFragment extends DialogFragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStyle(DialogFragment.STYLE_NORMAL, R.style.BottomDialog);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Window window = Objects.requireNonNull(getDialog().getWindow());
+        window.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        window.setBackgroundDrawable(null);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater,
+            @Nullable ViewGroup container,
+            @Nullable Bundle savedInstanceState) {
+        getDialog().requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        Window window = Objects.requireNonNull(getDialog().getWindow());
+        WindowManager.LayoutParams lp = window.getAttributes();
+        lp.gravity = Gravity.BOTTOM;
+        lp.width = WindowManager.LayoutParams.MATCH_PARENT;
+        window.setAttributes(lp);
+
+        return onCreateContentView(inflater, container, savedInstanceState);
+    }
+
+    public abstract View onCreateContentView(
+            LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState);
+}
