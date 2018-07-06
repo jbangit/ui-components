ef0c8dd677573efc3d8c53ba8d22481ee5a7501b
yuanjunyao <yuanjunyao@jbangit.com>
2018-07-06 17:00:00 +0800

    feat(TextTab): TextTab更多选项

--- library/src/main/java/com/jbangit/uicomponents/tab/TextTab.java
+++ library/src/main/java/com/jbangit/uicomponents/tab/TextTab.java
@@ -5,25 +5,49 @@ import android.content.res.TypedArray;
 import android.support.annotation.ColorInt;
 import android.support.annotation.NonNull;
 import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.jbangit.uicomponents.R;
+import com.jbangit.uicomponents.common.DensityUtils;
+import com.jbangit.uicomponents.common.Globals;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class TextTab extends ViewTab {
 
+    public static final int STYLE_DRY = 0;
+
+    public static final int STYLE_FILL = 1;
+
+    public static final int STYLE_STRONG = 2;
+
     private List<CharSequence> mAttrTitles;
 
     @ColorInt
-    private int mColorTextUnselected;
+    private int mAttrSelectedTextColor = getResources().getColor(R.color.colorTextDark);
 
     @ColorInt
-    private int mColorTextSelected;
+    private int mAttrUnselectedTextColor = getResources().getColor(R.color.colorTextDark);
+
+    @ColorInt
+    private int mAttrSelectedColor;
+
+    @ColorInt
+    private int mAttrUnselectedColor;
+
+    private int mAttrVPadding;
+
+    private int mAttrHPadding;
+
+    private int mAttrTextSize = DensityUtils.getPxFromSp(getContext(), 16);
+
+    private int mAttrTextGravity = Gravity.CENTER;
 
     public TextTab(Context context) {
         super(context);
@@ -43,24 +67,85 @@ public class TextTab extends ViewTab {
         setCurrentItem(0, false);
     }
 
+    private void initAttrs(Context context, AttributeSet attrs) {
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.TextTab);
+
+        mAttrTitles = Arrays.asList(typedArray.getTextArray(R.styleable.TextTab_textTabTitles));
+
+        switch (typedArray.getInt(R.styleable.TextTab_textTabStyle, STYLE_DRY)) {
+            case STYLE_DRY:
+                mAttrUnselectedTextColor = getResources().getColor(R.color.colorTextGray);
+                break;
+            case STYLE_FILL:
+                mAttrSelectedColor = getResources().getColor(R.color.colorForeground);
+                break;
+            case STYLE_STRONG:
+                mAttrSelectedTextColor = getResources().getColor(R.color.colorForeground);
+                mAttrSelectedColor = Globals.getPrimaryColor(getContext());
+                break;
+        }
+
+        mAttrSelectedTextColor =
+                typedArray.getColor(
+                        R.styleable.TextTab_textTabSelectedTextColor, mAttrSelectedTextColor);
+        mAttrUnselectedTextColor =
+                typedArray.getColor(
+                        R.styleable.TextTab_textTabSelectedTextColor, mAttrUnselectedTextColor);
+
+        mAttrSelectedColor =
+                typedArray.getColor(R.styleable.TextTab_textTabSelectedColor, mAttrSelectedColor);
+        mAttrUnselectedColor =
+                typedArray.getColor(
+                        R.styleable.TextTab_textTabUnselectedColor, mAttrUnselectedColor);
+
+        mAttrVPadding =
+                typedArray.getDimensionPixelSize(
+                        R.styleable.TextTab_textTabVPadding, mAttrVPadding);
+        mAttrHPadding =
+                typedArray.getDimensionPixelSize(
+                        R.styleable.TextTab_textTabHPadding, mAttrHPadding);
+
+        mAttrTextSize =
+                typedArray.getDimensionPixelSize(
+                        R.styleable.TextTab_textTabTextSize, mAttrTextSize);
+
+        mAttrTextGravity =
+                typedArray.getInt(
+                        R.styleable.TextTab_textTabTextGravity, mAttrTextGravity);
+        typedArray.recycle();
+    }
+
     private void setupAdapter() {
         setAdapter(
                 new ViewTabAdapter() {
                     @Override
                     public View getItemView(@NonNull ViewGroup container, int position) {
-                        View item =
+                        View layout =
                                 LayoutInflater.from(getContext())
                                         .inflate(R.layout.view_item_text_tab, container, false);
-                        TextView title = item.findViewById(R.id.title);
-                        title.setTextColor(mColorTextUnselected);
+                        TextView title = layout.findViewById(R.id.title);
+                        title.setTextColor(mAttrUnselectedTextColor);
+                        title.setTextSize(TypedValue.COMPLEX_UNIT_PX, mAttrTextSize);
+                        title.setGravity(mAttrTextGravity);
+
+                        layout.setBackgroundColor(mAttrUnselectedColor);
+                        layout.setPadding(
+                                mAttrHPadding, mAttrVPadding, mAttrHPadding, mAttrVPadding);
+
                         title.setText(mAttrTitles.get(position));
-                        return item;
+
+                        return layout;
                     }
 
                     @Override
                     public void onSelected(@NonNull View item, int position, boolean selected) {
+                        item.setBackgroundColor(
+                                selected ? mAttrSelectedColor : mAttrUnselectedColor);
                         ((TextView) item.findViewById(R.id.title))
-                                .setTextColor(selected ? mColorTextSelected : mColorTextUnselected);
+                                .setTextColor(
+                                        selected
+                                                ? mAttrSelectedTextColor
+                                                : mAttrUnselectedTextColor);
                     }
 
                     @Override
@@ -68,16 +153,5 @@ public class TextTab extends ViewTab {
                         return mAttrTitles.size();
                     }
                 });
-    }
-
-    private void initAttrs(Context context, AttributeSet attrs) {
-        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.TextTab);
-
-        mAttrTitles = Arrays.asList(typedArray.getTextArray(R.styleable.TextTab_textTabTitles));
-
-        mColorTextUnselected = getContext().getResources().getColor(R.color.colorTextGray);
-        mColorTextSelected = getContext().getResources().getColor(R.color.colorTextDark);
-
-        typedArray.recycle();
     }
 }
